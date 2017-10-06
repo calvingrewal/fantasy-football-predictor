64b15ad30b9108f5928381f2311613896df9a652
Calvin Grewal <calvingrewal28@gmail.com>
2017-10-05 21:41:13 -0700

    api can return list of tweets and api rate status

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const { CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET } = process.env
+const express = require('express')
 const Twit = require('twit')
+
 const T = new Twit({
   consumer_key:         CONSUMER_KEY,
   consumer_secret:      CONSUMER_SECRET,
@@ -8,10 +10,31 @@ const T = new Twit({
   access_token_secret:  ACCESS_SECRET
 })
 
-async function getData() {
-  const data = await T.get('search/tweets', { q: 'alex+smith', count: 100 })
-  console.log(data)
+const app = express()
+
+async function getData(q, until) {
+  const data = await T.get('search/tweets', { q, until, count: 100, result_type: 'popular'})
+  return data
 }
 
-getData()
+app.get('/', async (req, res) => {
+  const data = await getData('alex+smith', '2017-10-06') 
+  const tweets = data.data.statuses.map(tweet => ({
+    text: tweet.text,
+    id: tweet.id,
+    date: tweet['created_at']
+  }))
 
+  return res.json({
+    tweets
+  })
+})
+
+app.get('/status', async (req, res) => {
+  const status = await T.get('application/rate_limit_status', {resources:'search'})
+  res.json(status)
+})
+
+app.listen(3000, () => {
+  console.log('**********listening on port 3000**********')
+})
